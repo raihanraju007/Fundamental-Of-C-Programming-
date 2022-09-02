93671c026c281941bc289c9a841e397d9991bc96
raihanraju007 <38502847+raihanraju007@users.noreply.github.com>
2022-09-02 08:47:29 +0600

    Add files via upload
    Special Programs in C - Check If The Number Is Palindrome Number

---
+++ Palindrome.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+int n, result = 0, q, rem;
+printf("Please enter the number: ");
+scanf("%d", &n);
+
+q = n;
+while(q != 0){
+    rem = q%10;
+    result = result*10 + rem;
+    q = q/10;
+}
+if(result == n)
+    printf("Its a palindrome");
+else
+    printf("No ! its not a palindrome");
+return 0;
+
+}
